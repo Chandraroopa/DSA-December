51405a5959cf595c08c98d7bcf54b2163ca4a447
Chandraroopa <acharyaanu407@gmail.com>
2025-12-24 15:59:46 +0530

    Create highest_altitude.java

---
+++ highest_altitude.java
@@ -0,0 +1,12 @@
+class Solution{
+  public static void main(String args[]){
+    int max = 0;
+    int current = 0;
+
+    for(int i = 0 ; i < gain.length; i++){
+      current += gain[i];
+      max = Math.max(max,current);
+    }
+  return max;
+ }
+}
