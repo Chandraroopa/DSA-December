bf0b2ec937722dc7e5ac7e5f30d459702d9d469d
Chandraroopa <acharyaanu407@gmail.com>
2026-01-10 22:55:32 +0530

    Created length_of_lastword.java

---
+++ length_of_lastword.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        int i = s.length() - 1;
+        while(i >= 0 && s.charAt(i) == ' ') 
+            if((s.charAt(i)) == ' ') i = i - 1;
+        while(i >= 0 && s.charAt(i) != ' '){
+            count++;
+            i--;
+        }
+        return count;
+    }
+}
